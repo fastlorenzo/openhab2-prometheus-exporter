9925a9469eb2a03c8e2376575ea2e6038d322c4b
fastlorenzo <git@bernardi.be>
2020-05-07 08:58:00 +0200

    Added things support

--- openhab2-exporter.py
+++ openhab2-exporter.py
@@ -2,8 +2,7 @@ import urllib.request
 import json
 import time
 
-
-def get_metrics():
+def get_items_metrics():
     url = urllib.request.urlopen('http://127.0.0.1:8080/rest/items?recursive=false&fields=name,state,type')
     content_bytes = url.read()
     content = content_bytes.decode('utf-8')
@@ -27,24 +26,50 @@ def get_metrics():
     return res
 
 
+def get_things_metrics():
+    url = urllib.request.urlopen('http://127.0.0.1:8080/rest/things?recursive=false')
+    content_bytes = url.read()
+    content = content_bytes.decode('utf-8')
+
+    url.close()
+
+    obj = json.loads(content)
+    ts = int(round(time.time() * 1000))
+
+    return print_metrics(obj, 'thing', ts)
+
+
 def print_metrics(metrics, type, timestamp):
     metric_name = 'openhab2_metric_' + type
 
     res = '# TYPE {} gauge\n'.format(metric_name)
 
-    for metric in metrics:
-        name = metric['name']
-        value = metric['state']
+    if type != 'thing':
+        for metric in metrics:
+            name = metric['name']
+            value = metric['state']
 
-        if value is None or value == 'NULL':
-            continue
+            if value is None or value == 'NULL':
+                continue
 
-        if metric['type'].lower() == 'switch':
-            value = 1 if value == 'ON' else 0
-        elif metric['type'].lower() == 'contact':
-            value = 1 if value == 'OPEN' else 0
+            if metric['type'].lower() == 'switch':
+                value = 1 if value == 'ON' else 0
+            elif metric['type'].lower() == 'contact':
+                value = 1 if value == 'OPEN' else 0
 
-        res = res + metric_name + '{name="' + name + '"} ' + '{} {}\n'.format(value, timestamp)
+            res = res + metric_name + '{name="' + name + '"} ' + '{} {}\n'.format(value, timestamp)
+
+    else:
+        for metric in metrics:
+            label = metric['label']
+            uid = metric['UID']
+            status = metric['statusInfo']['status']
+            statusDetail = metric['statusInfo']['statusDetail']
+
+            if status is None or status == 'NULL':
+                continue
+
+            res = res + metric_name + '{label="' + label + '", statusDetail="' + statusDetail + '", uid="' + uid + '"} ' + '{} {}\n'.format(status, timestamp)
 
     return res
 
@@ -53,8 +78,10 @@ def app(environ, start_response):
     """
     Entrypoint for gunicorn
     """
-    metrics = get_metrics()
-    data = metrics.encode('utf-8')
+    items_metrics = get_items_metrics()
+    data = items_metrics.encode('utf-8')
+    things_metrics = get_things_metrics()
+    data = data + things_metrics.encode('utf-8')
 
     start_response('200 OK', [
         ('Content-Type', 'text/plain'),
